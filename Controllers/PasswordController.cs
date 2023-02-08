8e12c46d552ba5c6e655d908fe0be7c38c97e48c
Robin-UV <RJamesUV@outlook.com>
2023-02-07 22:17:32 -0500

    Initial commit

---
+++ Controllers/PasswordController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using RandomPasswordGeneratorMvc.Models; 
+
+namespace RandomPasswordGeneratorMvc.Controllers
+{
+    public class PasswordController : Controller
+    {
+        // GET: /<controller>/
+        [HttpGet]
+        public IActionResult Index()
+        {
+            PasswordViewModel model = new PasswordViewModel(); 
+            return View(model);
+        }
+
+        // Function to generate new random password
+
+        [HttpPost]
+        public IActionResult GenerateNewRandomPassword(int passwordLength, bool includeUpperCase, bool includeSpecialCharacters)
+        {
+            PasswordViewModel model = new PasswordViewModel();
+            Random random = new Random();
+
+            string defaultPassword = "abcdefghijklmnopqrstuvwxyz0123456789";
+            string upperCase = "ABCDEFGHIJKLMNOPQRSTUVQXYZ";
+            string specialCharacters = "!@#$%^&*()?";
+            string generatedPassword = "";
+            string result = "";
+
+            if (includeUpperCase && includeSpecialCharacters)
+            {
+                generatedPassword = defaultPassword + upperCase + specialCharacters; 
+            }
+            else if (includeUpperCase)
+            {
+                generatedPassword = defaultPassword + upperCase; 
+            }
+            else if (includeSpecialCharacters)
+            {
+                generatedPassword = defaultPassword + specialCharacters; 
+            }
+            else
+            {
+                generatedPassword = defaultPassword; 
+            }
+
+            for (int i = 0; i < passwordLength; i++)
+            {
+                int letterNumber = random.Next(0, generatedPassword.Length);
+                char letter = generatedPassword[letterNumber];
+                result += letter; 
+            }
+
+            model.GeneratedPassword = result;
+
+            return View("Index", model); 
+        }
+    }
+}
+
---
+++ Models/PasswordViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace RandomPasswordGeneratorMvc.Models
+{
+	public class PasswordViewModel
+	{
+		public string GeneratedPassword { get; set; } = null!;
+		public int PasswordLength { get; set; }
+		public bool IncludeUpperCase { get; set; }
+		public bool IncludeSpecialCharacters { get; set; }
+	}
+}
+
